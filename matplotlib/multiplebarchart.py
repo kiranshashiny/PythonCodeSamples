26b039bf42826fc7ef7682a0f07d1e147e6a80a5
Shashi Kiran <kiranshashiny@gmail.com>
2018-04-19 17:00:35 +0530

    Init

---
+++ matplotlib/multiplebarchart.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+x = [1, 4, 8, 12]
+y = [12,2,10,4]
+
+x2 = [ 3,5,6,7]
+y2 = [ 8,9,10,11 ]
+
+plt.bar (x, y )
+plt.bar (x2, y2 )
+plt.show()
