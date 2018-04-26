d1bf8c4172608c7013d1616b12a9c88705cc70a7
Shashi Kiran <kiranshashiny@gmail.com>
2018-04-26 16:16:29 +0530

    Updated

--- pandas/readcsv.py
+++ pandas/readcsv.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
-broken_df = pd.read_csv('HON.csv')
+df = pd.read_csv('HON.csv')
+#This prints everything to everything.
+
+
+# This reads in the column names as well, and assigns the column names
+df = pd.read_csv('HON.csv', names=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume' ])
+
+#This prints as a Series in Pandas. one after another 
+print ( df['Date'])
+
+# This prints as a numpy array.  All in one line.
+print ( df['Date'].values )
+
+#print ("Printing the Open now " )
+#print ( df['Open'].values )
 
